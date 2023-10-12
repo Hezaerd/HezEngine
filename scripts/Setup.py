68239eb054d1104997e5c4dbcc38a02d85c7ba9b
Swann <goldenland046@gmail.com>
2023-10-12 08:11:38 +0200

    update submodule and generate project from python script

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import platform
+import time
 
 from SetupPython import PythonConfiguration as PythonConfig
 
@@ -11,3 +12,17 @@ from SetupPremake import PremakeConfiguration as PremakeConfig
 os.chdir('./../') # Move to the root directory
 
 premakeInstalled = PremakeConfig.Validate()
+
+elapsed = time.time()
+print("\nUpdating submodules...")
+subprocess.run(["git", "submodule", "update", "--init", "--recursive"])
+print(f"Submodules updated in {time.time() - elapsed} seconds")
+
+if (premakeInstalled):
+    if platform.system() == "Windows":
+        print("\nGenerating Visual Studio 2022 project files...")
+        subprocess.call([os.path.abspath("./scripts/GenerateProjects.bat"), "nopause"])
+
+        print("\nSetup completed!")
+    else:
+        print("\nHezEngine requires Premake5 to generate project files.")
